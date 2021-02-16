d61541030a86bdfe3812f32b640002cb9287a997
kylest0kes <kylestokes1551@gmail.com>
2021-02-15 20:26:33 -0500

    made sign up form

---
+++ script.js
@@ -0,0 +1,7 @@
+// for sign up form animation
+const button = document.querySelector('.btn')
+const form   = document.querySelector('.form')
+
+button.addEventListener('click', function() {
+   form.classList.add('form--no') 
+});
